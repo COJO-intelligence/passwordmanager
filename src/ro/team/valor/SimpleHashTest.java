51550fd6e42c33ef7ab6a087f98ca4d495717900
Alexandru Costake <alexandrucostake@gmail.com>
2020-06-06 22:59:58 +0300

    Testing the git interaction from IntelliJ with a simple hashing class

---
+++ src/ro/team/valor/SimpleHashTest.java
@@ -0,0 +1,44 @@
+package ro.team.valor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SimpleHashTest {
+    private static void printHex(byte[] hashValues, String algorithm) {
+        System.out.print(algorithm + ": ");
+        for(byte hashValue:hashValues) {
+            System.out.printf("%02X", hashValue);
+        }
+        System.out.println(". The length of the hash is " + hashValues.length * 8 + ";");
+    }
+
+    private static void applyHash(byte[] inputBytes,String algorithm) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        byte[] outputBytes = md.digest(inputBytes);
+        printHex(outputBytes, algorithm);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--------------------START--------------------");
+        try {
+            File inputFile = new File("src/ro/team/valor/testFile.txt");
+            FileInputStream fis = new FileInputStream(inputFile);
+            byte[] inputBytes = fis.readAllBytes();
+            System.out.println("The hashes of the file are:");
+            applyHash(inputBytes, "MD5");
+            applyHash(inputBytes, "SHA-1");
+            applyHash(inputBytes, "SHA-256");
+            applyHash(inputBytes, "SHA-512");
+        } catch(IOException exception) {
+        System.out.println("Something is wrong with the file!");
+        exception.printStackTrace();
+        } catch(NoSuchAlgorithmException exception) {
+            System.out.println("Something is wrong with the hash algorithm!");
+            exception.printStackTrace();
+        }
+        System.out.println("---------------------STOP--------------------");
+    }
+}
