6682c3c2dbcfe221397c3a3c72f6ba0f44d22024
SuperGx <mitreageorge14@stud.ase.ro>
2020-08-14 18:51:30 +0300

    Frame management nou

--- src/main/java/gui/login/MainUI.java
+++ src/main/java/gui/login/MainUI.java
@@ -3,6 +3,7 @@ package main.java.gui.login;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import main.java.gui.storage.storageMain;
 import main.java.login.Login;
 
 import javax.swing.*;
@@ -14,16 +15,37 @@ public class MainUI {
 
     public static void main(String[] args) {
         FlatDarculaLaf.install();
+        JFrame frame = new JFrame("PASSWORD MANAGER");
         try {
             Login login = new Login();
             if (login.isPasswordSet())
             {
-                startLoginFrame();
+                startLoginFrame(frame);
             }
             else
             {
-                startSetPassFrame();
+                startSetPassFrame(frame);
             }
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+            frame.setResizable(false);
+            frame.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                    storageMain storageGUI = new storageMain("test.encrypt");
+                    storageGUI.writeListPanel("test.encrypt");
+                    if (JOptionPane.showConfirmDialog(frame,
+                            "The passwords were saved!", "Success!",
+                            JOptionPane.YES_NO_OPTION,
+                            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                        System.exit(0);
+
+                    }
+                }
+            });
+
         } catch (NoSuchAlgorithmException | IOException e) {
             e.printStackTrace();
         }
@@ -35,27 +57,17 @@ public class MainUI {
 
     }
 
-    private static void startLoginFrame()
+    private static void startLoginFrame(JFrame frame)
     {
-        JFrame frame = new JFrame("PASSWORD MANAGER");
         LoginUI loginUI = new LoginUI();
         frame.setContentPane(loginUI.getMainPanel());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(false);
         frame.getRootPane().setDefaultButton(loginUI.getLoginButton());
     }
 
-    private static void startSetPassFrame()
+    private static void startSetPassFrame(JFrame frame)
     {
-        JFrame frame = new JFrame("PASSWORD MANAGER");
         SetPassUI setPassUI = new SetPassUI();
         frame.setContentPane(setPassUI.getPassPanel());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(false);
         frame.getRootPane().setDefaultButton(setPassUI.getLoginButton());
     }
 }
